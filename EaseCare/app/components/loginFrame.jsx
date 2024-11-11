ce4c8079eeb5e27e2e69bf54472d3ae8d0ee9e84
Kent Phillip Carreon <kentphillipc@gmail.com>
2024-11-11 21:03:10 +0800

    Updated the Log in Page UI

--- EaseCare/app/components/loginFrame.jsx
+++ EaseCare/app/components/loginFrame.jsx
@@ -1,61 +1,98 @@
 import React, { useState } from 'react';
-import { View, Image, Alert } from 'react-native';
+import { View, Image, Alert, TouchableOpacity, Platform } from 'react-native';
 import { Text, TextInput, Button } from 'react-native-paper';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
 import styles from '../styles/loginStyle';
 
-const LoginScreen = ({ navigation }) => {
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
+
+const HoverableButton = ({ children, onPress, style, labelStyle, mode }) => {
+    const [isPressed, setIsPressed] = useState(false);
 
     return (
-    <View style={styles.container}>
-        <View style={styles.logoContainer}>
-        <Image
-            source={require('./assets/logo.png')}style={styles.logo}/>
-        <Text style={styles.title}>EASECARE</Text>
+        <Button
+            mode={mode}
+            onPress={onPress}
+            style={[style,isPressed && styles.pressedButton,]}
+            labelStyle={labelStyle}
+            onPressIn={() => setIsPressed(true)}
+            onPressOut={() => setIsPressed(false)}>{children}
+        </Button>
+    );
+};
+
+const LoginScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleBiometricsLogin = () => {
+        // Implement biometric login logic here
+        console.log('Biometric login initiated');
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.logoContainer}>
+                <Image source={require('./assets/logo.png')} style={styles.logo} />
+                <Text style={styles.title}>EASECARE</Text>
+            </View>
+
+            <TextInput
+                label="Email"
+                mode="outlined"
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+                style={styles.input}
+                left={<TextInput.Icon icon={() => <Ionicons name="mail" size={20} color="#6e6e6e" />} />}/>
+
+            <TextInput
+                label="Password"
+                mode="outlined"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+                style={styles.input}
+                left={<TextInput.Icon icon={() => <Ionicons name="lock-closed" size={20} color="#6e6e6e" />} />}/>
+
+            <Button
+                mode="text"
+                style={styles.forgotPasswordButton}
+                labelStyle={styles.forgotPasswordLabel}
+                onPress={() => navigation.navigate('ForgotPasswordScreen')}>Forgot Password?
+            </Button>
+
+            <HoverableButton
+                mode="contained"
+                style={styles.loginButton}
+                labelStyle={styles.loginButtonText}
+                onPress={() => Alert.alert('Log In', 'Success!')}>Log In
+            </HoverableButton>
+            <View style={styles.separator} />
+
+            <View style={styles.socialLoginContainer}>
+                    <TouchableOpacity onPress={handleBiometricsLogin} style={styles.biometricsButton}>
+                        <Text style={styles.biometricLabel}>Login using <Text style={styles.biometricBold}>BIOMETRICS</Text></Text>
+                    </TouchableOpacity>
+                <Text style={styles.orText}>OR</Text>
+                <View style={styles.socialButtonsContainer}>
+                    <TouchableOpacity style={styles.socialButton}>
+                        <FontAwesome name="facebook" size={24} color="#3b5998" />
+                        <Text style={styles.socialButtonText}>Facebook</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.socialButton}>
+                        <FontAwesome name="google" size={24} color="#db4437" />
+                        <Text style={styles.socialButtonText}>Google</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <Button
+                mode="text"
+                style={styles.registerButton}
+                labelStyle={{ color: 'black' }}
+                onPress={() => navigation.navigate('SignUp')}>Don't have an account? <Text style={styles.signUpText}>Sign Up</Text>
+            </Button>
         </View>
-
-        <TextInput
-        label="Email"
-        mode="outlined"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-        autoCapitalize="none"
-        style={styles.input}
-        left={<TextInput.Icon icon={() => <Ionicons name="mail" size={20} color="#6e6e6e" />} />}/>  
-
-        <TextInput
-        label="Password"
-        mode="outlined" 
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-        style={styles.input}
-        left={<TextInput.Icon icon={() => <Ionicons name="lock-closed" size={20} color="#6e6e6e" />} />}/>
-
-        <Button
-        mode="text"
-        style={styles.forgotPasswordButton}
-        labelStyle={styles.forgotPasswordLabel}
-        onPress={() => navigation.navigate('ForgotPasswordScreen')}>Forgot Password?
-        </Button>
-        
-        <Button
-        mode="contained"
-        style={styles.loginButton}
-        onPress={() => Alert.alert('Log In', 'Success!')}>Log In
-        </Button>
-
-        <Button
-        mode="text"
-        style={styles.registerButton}
-        labelStyle={{ color: 'black' }}
-        onPress={() => navigation.navigate('SignUp')}>Don't have an account? 
-        <Text style={styles.signUpText}> Sign Up</Text> 
-        </Button>
-    </View>
     );
 };
 
--- EaseCare/app/styles/loginStyle.jsx
+++ EaseCare/app/styles/loginStyle.jsx
@@ -14,12 +14,12 @@ const styles = StyleSheet.create({
         marginBottom: 40,
     },
     logo: {
-        width: 70, 
-        height: 55, 
+        width: 100, 
+        height: 80, 
         marginRight: 10,
     },
     title: {
-        fontSize: 32,
+        fontSize: 45,
         fontWeight: 'bold',
         color: '#333',
     },
@@ -31,17 +31,80 @@ const styles = StyleSheet.create({
         marginBottom: 20,
         marginTop: -20,
     },
+    pressedButton: {
+        backgroundColor: '#4A90E2', 
+    },
     forgotPasswordLabel: {
         color: '#D97706',
-        },
+        fontWeight: '500',
+    },
     loginButton: {
-        marginTop: 20,
-        backgroundColor: '#007AFF',
+        backgroundColor: '#007bff',
+        borderRadius: 5,
+    },
+    pressedButton: {
+        backgroundColor: '#D97706', 
+    },
+    loginButtonText: {
+        color: '#ffffff',
+        fontWeight: 'bold',
+    },
+    separator: {
+        height: 2, 
+        backgroundColor: 'black', 
+        width: '100%',
+        marginVertical: 20,
+        borderColor: '#333333', 
+        borderWidth: 1,
+        opacity: 0.3, 
+    },
+    socialLoginContainer: {
+        marginTop: 15,
+        alignItems: 'center',
+        marginVertical: 20,
+    },
+    biometricLabel: {
+        fontSize: 14,
+        color: '#D97706',
+        fontWeight: 'bold',
+    },
+    biometricBold: {
+        fontWeight: 'bold',
+        color: '#D97706',
+    },
+    orText: {
+        fontSize: 14,
+        color: '#666',
+        marginVertical: 10,
+    },
+    socialButtonsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '100%',
+        marginTop: 10,
+    },
+    socialButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderColor: '#ccc',
+        borderWidth: 1,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+        marginHorizontal: 10,
+    },
+    pressedButton: {
+        backgroundColor: '#D97706', 
+    },
+    socialButtonText: {
+        marginLeft: 5,
+        color: '#333',
     },
     registerButton: {
         marginTop: 20,
     },
     signUpText: {
+        fontSize: 17,
         color: '#D97706',
         fontWeight: 'bold', 
     },
